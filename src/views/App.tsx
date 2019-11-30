f106bc7463e2541eda471db90726c9566434bb0b
huoxiaoye <17711034@bjtu.edu.cn>
2019-11-30 19:18:21 +0800

    detail

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "../assets/css/app-reset.css"
-import { BrowserRouter as Router, Link, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import List from "./list/list"
 import 'antd/dist/antd.css'
 import Detail from "./detail/detail"
